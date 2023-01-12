e6e42d74496b382f8fa80cf3fffc742df402e8a9
aniketvadi <vadianiket@gmail.com>
2023-01-12 11:08:56 +0530

    add firebase

--- lib/controller/fire_base_controller.dart
+++ lib/controller/fire_base_controller.dart
@@ -1,8 +1,20 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
+import 'package:live_tv/modal/category_modal.dart';
 
 class FirebaseController extends GetxController {
+  var categoryList = <CategoryModal>[].obs;
+
   Future<void> getCategory() async {
-    var querySnapshot = await FirebaseFirestore.instance.collection('').get();
+    var querySnapshot =
+        await FirebaseFirestore.instance.collection('category').get();
+        print(querySnapshot.docs[0]);
+
+    // var list =
+    //     querySnapshot.docs.map((doc) => CategoryModal.fromJson(doc)).toList();
+
+    // if (categoryList.isEmpty) {
+    //   categoryList.value = list;
+    // }
   }
 }
